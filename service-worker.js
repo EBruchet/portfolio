5616f86fef20884556e515956f0903e353441e20
Evan Bruchet <evan.bruchet@mail.mcgill.ca>
2019-06-21 17:07:33 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.6d75ccf24513c72ce411ae601e448c4e.js"
+  "/portfolio/precache-manifest.9a0d691216759d1a5ddbab0c5ac84beb.js"
 );
 
 workbox.clientsClaim();
